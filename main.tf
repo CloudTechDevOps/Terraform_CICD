f22848bd6cb5bfae85b238232388c230c338cdc3
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-02-28 16:08:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "dev" {
     ami = "ami-0440d3b780d96b29d"
     instance_type = "t2.micro"
+
     key_name = "jenkin"
     tags = {
       Name = "dev-ec2"
