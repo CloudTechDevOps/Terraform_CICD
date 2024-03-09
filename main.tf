7ba72baf43d1aebbdc3b3b2609294a1fe0853fbd
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-03-09 08:46:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "dev" {
+resource "aws_instance" "test" {
     ami = "ami-0440d3b780d96b29d"
     instance_type = "t2.medium"
     tags = {
