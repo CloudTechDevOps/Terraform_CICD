3801f88c9f79329665cbd553a4fc4b97d7e7110f
Sathwik <162279235+SreeSathwik@users.noreply.github.com>
2024-03-23 12:16:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-0440d3b780d96b29d"
+resource "aws_instance" "myins" {
+    ami = "ami-05295b6e6c790593e"
     instance_type = "t2.medium"
     tags = {
-      Name = "dev-ec2"
+      Name = "Sathwik-ec2"
     }
 }
