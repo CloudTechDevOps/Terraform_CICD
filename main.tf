bab1215b7c1a65b60cfd9d1152ed813110462d6c
Sathwik <162279235+SreeSathwik@users.noreply.github.com>
2024-03-27 12:50:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "myins" {
     ami = "ami-05295b6e6c790593e"
-    instance_type = "t2.medium"
+    instance_type = "t2.micro"
     tags = {
       Name = "Sathwik-ec2"
     }
