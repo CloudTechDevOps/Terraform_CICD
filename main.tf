1901fe6050717881c53a94741c6bc9e57967e49d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-04-01 09:15:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     ami = "ami-0440d3b780d96b29d"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
     tags = {
       Name = "dev-ec3"
     }
