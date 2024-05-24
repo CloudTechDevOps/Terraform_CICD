dbf80f5c6eeb347086352a62a98ae4d5e83b5b74
meenigenikhitha <168175568+meenigenikhitha@users.noreply.github.com>
2024-05-24 15:34:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0440d3b780d96b29d"
+    ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.nano"
     tags = {
       Name = "dev-ec2"
