702baa85d4d13606a0665ec57e172ea6dc3571e2
chintu-cloud <github.chintu02@gmail.com>
2025-11-19 14:49:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    ami = "ami-0fa3fe0fa7920f68e"
+    instance_type = "t3.micro"
     tags = {
       Name = "prod"
     }
